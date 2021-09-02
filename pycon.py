f130f70e48e7e579da5333a1106cc6eb68bebc50
suryaravikumar-pixer <suryaravikumar5@gmail.com>
2021-09-02 18:25:03 +0530

    what else we can do with python

---
+++ pycon.py
@@ -0,0 +1,6 @@
+print("hello pooja,\
+    with python u can work with \
+        AI, Machine learning \
+            Webdevelopment(django) \
+                Robitics(say drones) \
+                    etc")
